0dd388d6e3e5894816f767db05c068b73549dfc0
Marek Szymański <marek.w.szymanski@gmail.com>
2021-09-10 16:53:01 +0200

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Marek Szymanski on 10/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
